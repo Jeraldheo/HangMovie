7bed70cee1ae1b2849acd912da35db0a49e684f1
Jeraldheo <jeyson.marin@unah.hn>
2022-10-29 20:36:43 -0600

    Finished counting number of movies

---
+++ Hang_Movie.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class Hang_Movie
+{
+    public static void main(String[] args) throws Exception{
+        File movies = new File("movies.txt");
+        Integer num_movies = 0;
+        
+
+
+    }
+}
