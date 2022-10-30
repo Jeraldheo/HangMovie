c59fe5c6fa1ec658f40407848b092a7d4be85837
Jeraldheo <jeyson.marin@unah.hn>
2022-10-29 20:37:29 -0600

    Finished counting number of movies

--- Hang_Movie.java
+++ Hang_Movie.java
@@ -1,10 +1,20 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Hang_Movie
 {
     public static void main(String[] args) throws Exception{
         File movies = new File("movies.txt");
         Integer num_movies = 0;
+
+        Scanner read_movies = new Scanner(movies);
+
+        while(read_movies.hasNextLine())
+        {
+            num_movies++;
+            read_movies.nextLine();
+        }
+
         
 
 
