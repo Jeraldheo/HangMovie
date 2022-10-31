f21cebbd474ab2ebff321fb7ee5c6624f775577d
Jeraldheo <jeyson.marin@unah.hn>
2022-10-30 22:56:30 -0600

    Finished game but need to be divided into classes for better organization.

--- Hang_Movie.java
+++ Hang_Movie.java
@@ -1,16 +1,70 @@
+//TODO Build a class that can be used to update the progress the player makes and to check if an input is part of our chosen movie
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hang_Movie
 {
     public static void main(String[] args) throws Exception{
+        Scanner read_guess = new Scanner(System.in);
         File movies = new File("movies.txt");
         Integer num_movies = count_movies(movies);
         String chosen_movie = choose_movie(movies, num_movies);
-        
-        
+        Integer title_size = chosen_movie.length();
+        Character[] progress_display = new Character[title_size];
+        Hashtable<Character, ArrayList<Integer>>  letter_indexes  =  new Hashtable<Character, ArrayList<Integer>>();
 
+        final Integer CHANCES = 10;
+        Integer  current_chance = 0;
+        
+        for(int i = 0; i<title_size; i++)
+        {
+            Character  character = chosen_movie.charAt(i); 
+            if(Character.isLetter(character))
+            {
+                progress_display[i] = '_';
+
+                if(letter_indexes.containsKey(character))
+                {
+                    letter_indexes.get(character).add(i);
+                }
+                else
+                {
+                    ArrayList<Integer> indexes = new ArrayList<Integer>();
+                    indexes.add(i);
+                    letter_indexes.put(character, indexes);
+                }
+            }
+            else
+            {
+               progress_display[i] = ' ';
+            }
+         
+        }
+
+        
+        print_progress(progress_display);
+        do
+        {
+            System.out.print("Enter letter: ");
+            Character guess = read_guess.nextLine().charAt(0);
+
+            if(letter_indexes.containsKey(guess))
+            {
+                ArrayList<Integer> indexes = letter_indexes.get(guess);
+                for(Integer index: indexes)
+                {
+                    progress_display[index] = chosen_movie.charAt(index);
+                }
+                letter_indexes.remove(guess);
+            }
+
+            current_chance++;
+            print_progress(progress_display);
+        }while(!letter_indexes.isEmpty() && current_chance<CHANCES);
 
     }
 
@@ -41,5 +95,14 @@ public class Hang_Movie
         read_movies.close();
         return chosen_movie;
 
+    }
+
+    private static void print_progress(Character[] progress_display)
+    {
+        for(Character character: progress_display)
+        {
+            System.out.print(character + " ");
+        }
+        System.out.println();
     }
 }
